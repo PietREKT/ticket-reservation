8938b46e69f1e937302fa9a322501e6592976163
PietREKT <piotr_stoinski02@wp.pl>
2025-11-24 13:56:55 +0100

    Usunięto .env z poprzedniego commita

---
+++ tickets-backend/src/test/java/org/piet/ticketsbackend/TicketsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.piet.ticketsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

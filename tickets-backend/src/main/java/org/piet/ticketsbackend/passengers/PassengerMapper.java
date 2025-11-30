4c5ddbb9d0a83ffedbcf99e872329c7f1e15b5a3
Marcin Szczupak <marcin.108@interia.eu>
2025-11-30 14:09:42 +0100

    FIX: Passenger dziedziczy po User

--- tickets-backend/src/main/java/org/piet/ticketsbackend/passengers/PassengerMapper.java
+++ tickets-backend/src/main/java/org/piet/ticketsbackend/passengers/PassengerMapper.java
@@ -1,25 +1,43 @@
 package org.piet.ticketsbackend.passengers;
 
+import org.piet.ticketsbackend.passengers.dto.PassengerRequest;
 import org.piet.ticketsbackend.passengers.dto.PassengerResponse;
+import org.piet.ticketsbackend.users.entities.Role;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PassengerMapper {
 
-    public PassengerResponse toResponse(PassengerEntity p) {
+    public PassengerResponse toResponse(PassengerEntity entity) {
         PassengerResponse dto = new PassengerResponse();
+        dto.setId(entity.getId());
+        dto.setUsername(entity.getUsername());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setEmail(entity.getEmail());
+        dto.setBirthDate(entity.getBirthDate());
+        dto.setDocumentNumber(entity.getDocumentNumber());
+        return dto;
+    }
 
-        dto.setId(p.getId());
-        dto.setFirstName(p.getFirstName());
-        dto.setLastName(p.getLastName());
-        dto.setEmail(p.getEmail());
-        dto.setBirthDate(p.getBirthDate());
-        dto.setDocumentNumber(p.getDocumentNumber());
+    public PassengerEntity fromRequest(PassengerRequest request) {
+        PassengerEntity entity = new PassengerEntity();
+        copyToEntity(request, entity);
+        entity.setRole(Role.PASSENGER);
+        entity.setPasswordNeedsChanging(false);
+        return entity;
+    }
 
-        if (p.getUser() != null) {
-            dto.setUserId(p.getUser().getId());
+    public void copyToEntity(PassengerRequest request, PassengerEntity entity) {
+        // pola z User
+        if (request.getUsername() != null) {
+            entity.setUsername(request.getUsername());
         }
 
-        return dto;
+        entity.setFirstName(request.getFirstName());
+        entity.setLastName(request.getLastName());
+        entity.setEmail(request.getEmail());
+        entity.setBirthDate(request.getBirthDate());
+        entity.setDocumentNumber(request.getDocumentNumber());
     }
 }
